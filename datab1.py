0c4d80e6af796b61ca428079be7724e4a5a7c1ba
Sanshain <komissarovat8@gmail.com>
2022-01-26 20:20:42 +0300

    Opredelenie dnya nedeli i tekuschego vremeni

--- datab1.py
+++ datab1.py
@@ -1,14 +1,48 @@
+import datetime
+import re
+import calendar
+
 import mysql.connector
 from mysql.connector import Error
 
 
 import mysql.connector
+#def bazad:
 
+now = datetime.datetime.now()
+
+#@bot.message_handler(commands=['start'])
+
+ye=int(now.year)
+print(ye)
+chas=int(now.hour)
+min=int( now.minute)
+print('chas=',chas)
+print('min=',min)
+mes=int(now.month)
+chis=int(now.day)
+
+nomden=calendar.weekday(ye,mes,chis)
+kalen={}
+kalen={0:"mondey",
+         1:"tuesday",
+         2:"wednesday",
+         3:"thursday",
+         4:"friday"}
+den=kalen.get(nomden)
+
+
+
+
+
+    
 conn = mysql.connector.connect(
          user='root',
-         password='lizard',
+         #password='lizard',
          host='localhost',
-         database='tele')
+         database='rasp')
 
 
 cur = conn.cursor()
+print("cur=",cur)
+    
--- raspi1.py
+++ raspi1.py
@@ -4,7 +4,27 @@ import json
 from telebot import apihelper
 import datetime
 import re
+import calendar
 now = datetime.datetime.now()
 Token1="948381879:AAEO07_DU1z-CO67NRWIi8HX_jeLNfQVqPo" #Totochka
 bot = telebot.TeleBot(Token1)
-@bot.message_handler(commands=['start'])
+#@bot.message_handler(commands=['start'])
+
+ye=int(now.year)
+print(ye)
+chas=int(now.hour)
+min=int( now.minute)
+print('chas=',chas)
+print('min=',min)
+mes=int(now.month)
+chis=int(now.day)
+
+nomden=calendar.weekday(ye,mes,chis)
+kalen={}
+kalen={0:"mondey",
+         1:"tuesday",
+         2:"wednesday",
+         3:"thursday",
+         4:"friday"}
+den=kalen.get(nomden)
+print(den)
